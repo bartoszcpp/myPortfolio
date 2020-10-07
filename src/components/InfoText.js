57c0da87492e565ecce89241701d551c50c9fdd8
Bartosz Ciąpała <bartosz.cp@gmail.com>
2020-10-07 16:55:32 +0200

    edit first info text

--- src/components/InfoText.js
+++ src/components/InfoText.js
@@ -10,8 +10,12 @@ const InfoText = () => {
         stycznia tego roku rozpocząłem nauke biblioteki React. Wykonując kolejne
         zlecenia pogłębiałem dalszą wiedzę front-end ucząc się NextJS, Gatsby
         oraz GraphQL. Od czerwca pracuje przy komercyjnych projektach w firmie
-        KMB Studio, wykonując framework Gatsby. Szukam pracy na stanowisku
-        web-developera w okolicach Przemyśla lub zdalnie.
+        KMB Studio, wykorzystując framework Gatsby. Aktualnie piszę pracę
+        dyplomową, której tematem jest aplikacja mobilna w technologii React
+        Native. Postęp prac można obejrzeć na moim githubie (repozytorium
+        firstReactNativeApp). Szukam pracy na stanowisku web-developera w
+        okolicach Rzeszowa lub zdalnie. Nie boję się żadnych wyzwań i chętnie
+        podejmę naukę technologii, której jeszcze nie znam.
       </div>
     </>
   )
